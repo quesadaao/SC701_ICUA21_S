512ca544bfd72b580aab86a2890d2a94711c2d47
Oscar.Quesada <quesadaao@hotmail.com>
2021-02-27 08:57:46 -0600

    Commit de Oscar Soto

---
+++ Codigo_P1/Codigo_En_Clase/Solution_p1/Solution.DAL.Repository/IRepositoryGroupRequests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Solution.DO.Objects;
+
+namespace Solution.DAL.Repository
+{
+    // Interfase encargada de definir como vamos a implementar los metodos de include
+    // Esta misma Interfase implementa la interfase de repository para poder acceder a los metodos basicos
+    public interface IRepositoryGroupRequests:IRepository<GroupRequests>
+    {
+        Task<IEnumerable<GroupRequests>> GetAllWithAsync();
+        Task<GroupRequests> GetOneByIdWithAsync(int id);
+    }
+}
